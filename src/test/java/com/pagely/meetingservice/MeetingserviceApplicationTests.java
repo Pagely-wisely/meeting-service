045d50c2b7f84523694500bee9aee256317e71e5
hhegi <larbillilith@gmail.com>
2026-04-23 15:54:55 +0900

    🎉 Add: initialize project repository

---
+++ src/test/java/com/pagely/meetingservice/MeetingserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pagely.meetingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
