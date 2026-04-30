f1fdd85d9e6bda96636a3259a238e217a6faade4
m1nj1m <lovekmj23@gmail.com>
2026-05-01 02:24:55 +0900

    🍻 Test:  test 프로파일 추가 및 Kafka Listener 비활성화 설정 Related to: #59

--- src/test/java/com/pagely/meetingservice/MeetingserviceApplicationTests.java
+++ src/test/java/com/pagely/meetingservice/MeetingserviceApplicationTests.java
@@ -2,12 +2,13 @@ package com.pagely.meetingservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class MeetingserviceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
