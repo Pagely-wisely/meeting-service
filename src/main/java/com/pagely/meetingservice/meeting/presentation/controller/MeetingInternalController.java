4641c329979fd2ff808758f5878aa673921d727f
hhegi <larbillilith@gmail.com>
2026-05-01 15:01:36 +0900

    ♻️ Refactor: API 응답을 ApiResponse로 래핑

--- src/main/java/com/pagely/meetingservice/meeting/presentation/controller/MeetingInternalController.java
+++ src/main/java/com/pagely/meetingservice/meeting/presentation/controller/MeetingInternalController.java
@@ -1,7 +1,7 @@
 package com.pagely.meetingservice.meeting.presentation.controller;
 
+import com.pagely.common.response.ApiResponse;
 import com.pagely.meetingservice.meeting.application.service.MeetingInternalQueryService;
-import com.pagely.meetingservice.meeting.presentation.dto.response.InternalReadableMeetingsResponse;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -19,10 +19,8 @@ public class MeetingInternalController {
 
     // 유저 기준 열람 가능 모임 / 모임별 참가 일정 ID
     @GetMapping("/access/readable")
-    public ResponseEntity<InternalReadableMeetingsResponse> getReadableMeetings(@RequestParam UUID userId){
-        return ResponseEntity.ok(
-            meetingInternalQueryService.getReadableMeetings(userId)
-        );
+    public ResponseEntity<ApiResponse> getReadableMeetings(@RequestParam UUID userId) {
+        return ApiResponse.ok(meetingInternalQueryService.getReadableMeetings(userId));
     }
 }
 
